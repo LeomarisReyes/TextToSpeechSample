1366dfb0ca56515eeacf3bf5ea4c6be70a52d845
Leomaris Reyes <reyes.leomaris@gmail.com>
2018-06-13 21:14:02 -0400

    This is a TextToSpeech Plugin Sample.

---
+++ TextToSpeechSample/TextToSpeechSample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace TextToSpeechSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TextToSpeechSamplePage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ TextToSpeechSample/TextToSpeechSample/TextToSpeechSamplePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Plugin.TextToSpeech;
+using Xamarin.Forms;
+
+namespace TextToSpeechSample
+{
+    public partial class TextToSpeechSamplePage : ContentPage
+    {
+        public TextToSpeechSamplePage()
+        {
+            InitializeComponent();
+
+
+            if(Device.RuntimePlatform == Device.iOS)
+            {
+                this.Padding = new Thickness(0, 40, 0, 0);
+            } 
+
+        }
+         
+        private async void Speech_Cliked(object sender, EventArgs e)
+        {
+            //await CrossTextToSpeech.Current.Speak(TextToSpeech.Text,
+                                                  //null,
+                                                  //(float)sldTone.Value,
+                                                  //(float)sldSpeed.Value,
+                                                  //(float)sldVolume.Value);
+
+            await CrossTextToSpeech.Current.Speak(TextToSpeech.Text,
+                                                  null,
+                                                  (float)sldTone.Value,
+                                                  (float)sldSpeed.Value,
+                                                  (float)sldVolume.Value);
+        }
+    }
+}
